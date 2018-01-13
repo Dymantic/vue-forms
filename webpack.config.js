581f8de72e2e6bf5e9efd97f9e74bfb64c3f408a
Michael Joyner <joyner.michael@gmail.com>
2018-01-13 12:44:34 +0800

    add dist and bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
+const path = require("path");
+
 module.exports = {
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-                    presets: [
-                        ['env', {
-                            'modules': false,
-                            'targets': {
-                                'browsers': ['> 2%'],
-                                uglify: true
-                            }
-                        }]
-                    ],
-                    plugins: ['transform-object-rest-spread']
-              }
-            }
-          },
-          {
-            test: /\.vue$/,
-            use: 'vue-loader'
+  entry: path.resolve(__dirname + "/src/index.js"),
+  output: {
+    path: path.resolve(__dirname + "/dist/"),
+    filename: "index.js",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            presets: [
+              [
+                "env",
+                {
+                  modules: false,
+                  targets: {
+                    browsers: ["> 2%"],
+                    uglify: true
+                  }
+                }
+              ]
+            ],
+            plugins: ["transform-object-rest-spread"]
           }
-        ]
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: "vue-loader"
       }
-}
+    ]
+  }
+};
